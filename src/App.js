3a6f1f02fb41006ef0775d22320000229bf6a7ca
Rachel Chen <85042944+rachelchxn@users.noreply.github.com>
2024-02-17 19:01:59 -0500

    styling changes + menu bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import './App.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { HashRouter as Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   const openInNewTab = (url) => {
-    window.open(url, '_blank', 'noreferrer');
+    window.open(url, "_blank", "noreferrer");
   };
 
   return (
@@ -14,34 +14,39 @@ function App() {
       <div className="App">
         <Navbar />
         <main>
-          <section id="home">
-            <h1>Product Design Sprint</h1>
-            <p>March 8-10, 2024 | Hybrid Event - Virtual & Western University</p>
-            <br/>
-            <p>Develop your product management, design thinking, and prototyping</p>
+          <section className="section" id="home">
+            <h1>Product Design Sprint 2024</h1>
+            <p>
+              March 8-10, 2024 | Hybrid Event - Virtual & Western University
+            </p>
+            <br />
+            <p>
+              Develop your product management, design thinking, and prototyping
+            </p>
             <p>skills in a fun weekend-long competition.</p>
-            <br/>
+            <br />
             <p>Open to all levels. Participate from anywhere in the world.</p>
             <button
-            role="link"
-            onClick={() => openInNewTab('https://monkeytype.com/')}>
+              role="link"
+              onClick={() => openInNewTab("https://monkeytype.com/")}
+            >
               Apply Now!
-              </button>
-            <p>Applications close Jan 29, 2024.</p>
+            </button>
+            <p>Applications close March 4, 2024</p>
           </section>
-          <section id="about">
+          <section className="section" id="about">
             <h2>About</h2>
             <p>This is the about section.</p>
           </section>
-          <section id="partners">
+          <section className="section" id="partners">
             <h2>Partners</h2>
             <p>This is the partners section.</p>
           </section>
-          <section id="faq">
+          <section className="section" id="faq">
             <h2>FAQ</h2>
             <p>This is the FAQ section.</p>
           </section>
-          <section id="contact">
+          <section className="section" id="contact">
             <h2>Contact Us</h2>
             <p>This is the contact section.</p>
           </section>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,53 @@
-import React from 'react';
-import Logo from "../assets/pdsLogo.png";
-import { HashLink as Link } from 'react-router-hash-link'; // This will need to be installed
+import React, { useState } from "react";
+import Logo from "../assets/pds_logo.svg";
+import Menu from "../assets/menu.svg";
+import Close from "../assets/close.svg";
+import { HashLink as Link } from "react-router-hash-link"; // This will need to be installed
 import "../styles/Navbar.css"; // Make sure you have the correct path to your CSS file
 
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <nav className="navbar">
-        <div className="leftSide">
-            <img src={Logo} alt="PDS Logo" />
+      <div className="leftSide">
+        <img src={Logo} alt="PDS Logo" />
+      </div>
+      <div className="menu">
+        <img src={Menu} onClick={() => setShowMenu(true)} />
+      </div>
+      <div className="rightSide">
+        <Link smooth to="#about">
+          About
+        </Link>
+        <Link smooth to="#partners">
+          Partners
+        </Link>
+        <Link smooth to="#faq">
+          FAQ
+        </Link>
+        <Link className="button" smooth to="https://monkeytype.com/">
+          Apply Now!
+        </Link>
+      </div>
+      {showMenu && (
+        <div className="menu-list">
+          <img src={Close} onClick={() => setShowMenu(false)} />
+          <Link smooth to="#about">
+            About
+          </Link>
+          <Link smooth to="#partners">
+            Partners
+          </Link>
+          <Link smooth to="#faq">
+            FAQ
+          </Link>
+          <Link className="button" smooth to="https://monkeytype.com/">
+            Apply Now!
+          </Link>
         </div>
-        <div className="rightSide">
-            <Link smooth to="#about">About</Link>
-            <Link smooth to="#partners">Partners</Link>
-            <Link smooth to="#faq">FAQ</Link>
-            <Link smooth to="#contact">Contact Us</Link>
-        </div>
+      )}
     </nav>
-  )
+  );
 }
 
 export default Navbar;
